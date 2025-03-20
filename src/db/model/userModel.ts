6f026191d14f955fad9c6fd852bb3939a2c001ea
libin43 <libinbiji43@gmail.com>
2025-03-20 14:22:27 +0530

    feat: user model updated.

---
+++ src/db/model/userModel.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\d{10}$/, "Please enter a valid 10-digit mobile number"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address",
+      ],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model("users", userSchema);
